8912d3638a80f242045bbf553912791dac03f2f9
dcherepanov <dcherepanov9884@gmail.com>
2022-05-13 02:45:29 +0700

    6 questions and comments

--- question_4.py
+++ question_4.py
@@ -4,22 +4,11 @@ __author__ = 'Черепанов Дмитрий Евгеньевич'
 ''' 4. Пользователь вводит целое положительное число. Найдите самую большую цифру в числе.
 Для решения используйте цикл while и арифметические операции.'''
 
-str_number = input('Integer: ')
-max_number_while, i = 0, 0
-int_number = int(str_number)
-# while
+int_number = int(input('Integer: '))
+max_number, i = 0, 0
 while int_number > 0:
     last = int_number % 10
     int_number = int_number // 10
-    if max_number_while < last:
-        max_number_while = last
-print(f'Max number: {max_number_while}')
-max_number_for = 0
-# for
-for number in str_number:
-    if max_number_for < int(number):
-        max_number_for = int(number)
-print(f'Max number: {max_number_for}')
-# max
-max_number = max(str_number)
+    if max_number < last:
+        max_number = last
 print(f'Max number: {max_number}')
