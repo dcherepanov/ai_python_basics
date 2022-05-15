75fcc5f322f0e4b8a2b793c9f9fc7bd015ed3c37
dcherepanov <dcherepanov9884@gmail.com>
2022-05-15 15:33:02 +0700

    fixed 3 question

--- question_23.py
+++ question_23.py
@@ -10,6 +10,6 @@ month_number = int(input('Month number: '))
 print(seasons_list[month_number % 12 // 3])
 #через dict:
 seasons_dict = {'зима': [12, 1, 2], 'весна': [3, 4, 5], 'лето': [6, 7, 8], 'осень': [9, 10, 11]}
-for key_ in seasons_dict:
-    if month_number in seasons_dict[key_]:
-        print(key_)
+for key in seasons_dict:
+    if month_number in seasons_dict[key]:
+        print(key)
